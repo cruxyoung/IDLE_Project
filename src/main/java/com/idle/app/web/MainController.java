714d633488ff9f0b0d791f7887993cf685b3dd2d
xili9459 <xili9459@uni.sydney.edu.au>
2018-10-22 13:42:59 +1100

    Update src/main/java/com/idle/app/web/MainController.java

--- src/main/java/com/idle/app/web/MainController.java
+++ src/main/java/com/idle/app/web/MainController.java
@@ -1,25 +1,15 @@
 package com.idle.app.web;
 
-import java.io.IOException;
 import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.idle.app.common.ServerResponse;
-import com.idle.app.domain.User;
 import com.idle.app.service.UserManager;
 @Controller
 public class MainController {
-	@Autowired
-	private UserManager userManager;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
